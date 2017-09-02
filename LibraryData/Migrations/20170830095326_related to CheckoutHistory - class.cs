1b596e00912bec4c7c7369380d26380969c21933
Michael Trembovler <trembovler@outlook.com>
2017-09-02 07:51:14 +0300

    Add project files.

---
+++ LibraryData/Migrations/20170830095326_related to CheckoutHistory - class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryData.Migrations
+{
+    public partial class relatedtoCheckoutHistoryclass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CheckOut",
+                table: "CheckoutHistores",
+                newName: "CheckedOut");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CheckedOut",
+                table: "CheckoutHistores",
+                newName: "CheckOut");
+        }
+    }
+}
---
+++ LibraryData/Migrations/20170831145248_Hold-Branch-Patron-etc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryData.Migrations.LibraryDb
+{
+    public partial class HoldBranchPatronetc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
